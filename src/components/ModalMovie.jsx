8346010357844b232b161f271dbed9429fb3ed00
nostam <matsonho@gmail.com>
2020-11-23 18:14:21 +0100

    fix modal keep listening to render updates

--- src/components/ModalMovie.jsx
+++ src/components/ModalMovie.jsx
@@ -1,33 +1,32 @@
 import React, { Component } from "react";
 import { Modal, Button, Container, Col, Row } from "react-bootstrap";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
-import '../styles/ModalMovie.css'
+import "../styles/ModalMovie.css";
 import { Link, Route, Switch } from "react-router-dom";
-
-
-
 
 export default class ModalMovie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        currenntMovie: {},
-        currentMovieId:props.currenntMovie
-  };
+      currenntMovie: {},
+      currentMovieId: props.currenntMovie,
     };
+  }
 
+  //   componentDidMount = async (props) => {
+  //     console.log(this.state.currenntMovieId);
+  //     console.log(this.props.currenntMovie);
+  //   };
 
-//   componentDidMount = async (props) => {
-//     console.log(this.state.currenntMovieId);
-//     console.log(this.props.currenntMovie);
-//   };
-    
-    componentDidUpdate = async (prevProp,prevState) => {
-        if (prevProp.currentMovie !== this.props.currentMovie) {
-            let query = (this.props.currentMovie.join(''))
-                await this.fetchMovies(query);
-        }
+  componentDidUpdate = async (prevProp, prevState) => {
+    if (
+      this.props.currentMovie.length > 1 &&
+      prevProp.currentMovie !== this.props.currentMovie
+    ) {
+      let query = this.props.currentMovie.join("");
+      await this.fetchMovies(query);
     }
+  };
 
   fetchMovies = async (q) => {
     let baseUrl = `http://www.omdbapi.com/?`;
@@ -47,8 +46,8 @@ export default class ModalMovie extends Component {
     }
   };
   render() {
-      let { show, handleClose } = this.props;
-      let {currentMovie} = this.state
+    let { show, handleClose } = this.props;
+    let { currentMovie } = this.state;
     return (
       <Modal
         show={show}
@@ -80,7 +79,6 @@ export default class ModalMovie extends Component {
               </Col>
               <Col md={1} className="d-flex align-items-center">
                 <Link to={`/movie/${currentMovie?.imdbID}`}>
-                
                   <NavigateNextIcon
                     style={{ cursor: "pointer", fontSize: "30px" }}
                   />
@@ -101,10 +99,3 @@ export default class ModalMovie extends Component {
     );
   }
 }
-
-
-
-
-
-
-
